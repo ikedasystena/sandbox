4f68d146a0753fe302cf4266bea0b5085ec34f8c
ikedasystena <ikedah@systena.co.jp>
2017-11-09 13:58:09 +0900

    first comment

---
+++ twoThread.cpp
@@ -0,0 +1,35 @@
+#include<iostream>	//input,output
+#include<thread>	//thread
+#include<semaphore.h>	//semaphore
+
+//git練習用ソース
+
+//値を出力する関数
+void NumberOutput( int number, sem_t &ownNumberSem, sem_t &otherNumberSem ){
+
+    for( int i = number; i <= 10; i+=2 ){
+        sem_wait( &ownNumberSem );
+        std::cout << i << std::endl;
+        sem_post( &otherNumberSem );
+    }
+}
+
+int main( int argc, char *argv[] ){
+
+    //セマフォの作成
+    sem_t oddNumberSem;
+    sem_t evenNumberSem;
+    //セマフォの初期化
+    sem_init( &oddNumberSem, 0, 1 );
+    sem_init( &evenNumberSem, 0, 0 );
+
+    //スレッドの生成
+    std::thread oddNumberThread( NumberOutput, 1, std::ref( oddNumberSem ), std::ref( evenNumberSem ) );
+    std::thread evenNumberThread( NumberOutput, 2, std::ref( evenNumberSem ), std::ref( oddNumberSem ) );
+
+    //スレッドの終了待ち
+    oddNumberThread.join();
+    evenNumberThread.join();
+
+    return 0;
+}
